5c3277447e00316f609f3ed4a288c3bae463d5e2
Amir Daneshmand <amirdanesh02@gmail.com>
2024-10-05 02:21:37 +0330

    Improve variables typing

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import express from "express";
-import http from "http";
+import express, { Application } from "express";
+import http, { Server } from "http";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
 import compression from "compression";
@@ -8,17 +8,17 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const app: express.Application = express();
+const app: Application = express();
 
 app.use(cors({ credentials: true }));
 app.use(compression());
 app.use(cookieParser());
 app.use(bodyParser.json());
 
-const port: string | undefined = process.env.PORT;
-const localURL: string | undefined = process.env.LOCAL_URL;
+const port: string = process.env.PORT;
+const localURL: string = process.env.LOCAL_URL;
 
-const server = http.createServer(app);
+const server: Server = http.createServer(app);
 server.listen(port, (): void => {
   console.log(`The server is running on ${localURL}:${port}.`);
 });
