760490abd0f0ee53ce188c2370e1762ed5cea74f
Amir Daneshmand <amirdanesh02@gmail.com>
2024-10-16 01:31:25 +0330

    Complete all users fetch controller.

--- src/db/controllers/users-controllers.ts
+++ src/db/controllers/users-controllers.ts
@@ -9,6 +9,17 @@ export const getRegisteredUsers = async (
 ): Promise<Response> => {
   try {
     const users = await getAllUsers();
+
+    if (!users) {
+      return res
+        .status(404)
+        .json({ message: "No users found in the database." });
+    }
+
+    return res.status(200).json({
+      message: "Successfully fetched all registered users.",
+      data: users,
+    });
   } catch (error: unknown) {
     logControllerException("getRegisteredUsers", error as Error);
     return res.status(500).json({ message: "Internal server exception." });
