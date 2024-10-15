50e6dfbde4ffcf30e27dfdf17c8310562b410e69
Amir Daneshmand <amirdanesh02@gmail.com>
2024-10-15 16:28:40 +0330

    The same as the previous comment.

--- src/db/controllers/auth-controllers.ts
+++ src/db/controllers/auth-controllers.ts
@@ -1,1 +1,9 @@
 import { Request, Response } from "express";
+
+export const registerController = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  try {
+  } catch (error: unknown) {}
+};
