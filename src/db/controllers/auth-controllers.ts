cac4dcfc449f3794ca5f0131e738c235d2adcc06
Amir Daneshmand <amirdanesh02@gmail.com>
2024-10-15 21:19:24 +0330

    Add minor checking logic to register controller.

--- src/db/controllers/auth-controllers.ts
+++ src/db/controllers/auth-controllers.ts
@@ -1,9 +1,30 @@
 import { Request, Response } from "express";
 
+import { logControllerException } from "../../utils/controllers";
+
+interface RequestCredentials {
+  username: string;
+  email: string;
+  password: string;
+}
+
 export const registerController = async (
   req: Request,
   res: Response
 ): Promise<Response> => {
   try {
-  } catch (error: unknown) {}
+    // Get credentials sent by user from the request body,
+    const { username, email, password }: RequestCredentials = req.body;
+    // And then check for their presence:
+    if (!username || !email || !password) {
+      return res
+        .status(400)
+        .json({
+          message: "The username, email and password should be provided.",
+        });
+    }
+  } catch (error: unknown) {
+    logControllerException("registerController", error as Error);
+    return res.status(500).json({ message: "Internal server exception." });
+  }
 };
