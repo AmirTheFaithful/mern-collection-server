3f68dd4dc3e939bfbce2d3b807ec8c1406a93c87
Amir Daneshmand <amirdanesh02@gmail.com>
2024-10-30 20:15:13 +0330

    Add comment document scheme.

---
+++ src/db/schemas/comment-scheme.ts
@@ -0,0 +1,21 @@
+import { Schema } from "mongoose";
+import { ObjectId } from "bson";
+
+const CommentSchema = new Schema({
+  authorID: {
+    type: ObjectId,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  publicationDate: {
+    type: Date,
+    required: true,
+    default: Date.now(),
+  },
+  // Yet, it's all for now.
+});
+
+export default CommentSchema;
