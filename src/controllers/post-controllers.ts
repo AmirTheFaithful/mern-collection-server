6c65c1ddc94007957a5058e3f469b6cf3d27f2b3
Amir Daneshmand <amirdanesh02@gmail.com>
2024-11-11 16:39:09 +0330

    Add GET request controller.

--- src/controllers/post-controllers.ts
+++ src/controllers/post-controllers.ts
@@ -1,8 +1,25 @@
 import { Request, Response } from "express";
 import { ObjectId } from "mongodb";
 
-import { getPostById } from "../db/actions/posts-actions";
+import * as actions from "../db/actions/posts-actions";
 import { logControllerException, createObjId } from "../utils/controllers";
+
+export const getPosts = async (req: Request, res: Response): Promise<any> => {
+  try {
+    const posts = await actions.getAllPosts();
+
+    if (!posts || posts.length) {
+      return res.status(404).json({ message: "No posts were found." });
+    }
+
+    return res
+      .status(200)
+      .json({ data: posts, message: "Posts has been successfully fetched." });
+  } catch (error: unknown) {
+    logControllerException("getPosts", error as Error);
+    return res.status(500).json({ message: "Internal server error." });
+  }
+};
 
 export const getPost = async (req: Request, res: Response): Promise<any> => {
   try {
@@ -14,7 +31,7 @@ export const getPost = async (req: Request, res: Response): Promise<any> => {
 
     const id: ObjectId = createObjId(req.params.id);
 
-    const post = await getPostById(id);
+    const post = await actions.getPostById(id);
 
     if (!post) {
       return res.status(404).json({ message: "Comment not found." });
