5a5cdcf12ef57139b2e71da2d81d3531c4c2f022
Amir Daneshmand <amirdanesh02@gmail.com>
2024-11-15 21:07:54 +0330

    Add db action functions for  reaction documents.

---
+++ src/db/actions/reactions-actions.ts
@@ -0,0 +1,13 @@
+import { ObjectId } from "mongoose";
+
+import ReactionModel from "../models/reaction-model";
+
+export const getReactionById = (id: ObjectId) => ReactionModel.findById(id);
+export const getReactionsByPostId = (id: ObjectId) =>
+  ReactionModel.find({ postID: id });
+export const createNewReaction = (values: Record<string, any>) =>
+  new ReactionModel(values).save().then((reaction) => reaction.toObject());
+export const updateReactionById = (id: ObjectId, values: Record<string, any>) =>
+  ReactionModel.findByIdAndUpdate(id, values);
+export const deleteReactionById = (id: ObjectId) =>
+  ReactionModel.findByIdAndDelete(id);
