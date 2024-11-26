f07693b6b7a7d47b296e1339d7f3c9bef8b88825
Amir Daneshmand <amirdanesh02@gmail.com>
2024-11-26 21:13:09 +0330

    Set user document action function to find the user by email.

--- src/routers/users-router.ts
+++ src/routers/users-router.ts
@@ -3,9 +3,11 @@ import { Router } from "express";
 import {
   getRegisteredUser,
   getRegisteredUsers,
+  getRegisteredUserByEmail,
 } from "../controllers/users-controllers";
 
 export default (router: Router): void => {
   router.get("/api/users", getRegisteredUsers);
   router.get("/api/users/:id", getRegisteredUser);
+  router.get("/api/special/users/:email", getRegisteredUserByEmail);
 };
